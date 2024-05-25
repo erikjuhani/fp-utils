74a0e4830d22fbb3847356d6e4640a058ae04076
Erik Kinnunen <erik.kinn@gmail.com>
2024-05-25 13:40:36 +0300

    Add `deno task up` command to easily upgrade jsr dependencies
    The up command will lookup all deno.jsonc files and find the `jsr:`
    definitions. The command is formed by using common unix tools like
    `find`, `jq` and `xargs`. The command does not support ugrading to
    certain version and will always try to upgrade to the latest version of
    the dependency.
    
    For now only the dev dependencies will be properly upgraded and the lock
    file modified as deno supports only dependency management and caching
    from files. More
    [here](https://docs.deno.com/runtime/tutorials/manage_dependencies)
    
    Upgrade dev dependencies and add `dev_deps.ts` file for dependency
    management.

---
+++ dev_deps.ts
@@ -0,0 +1,11 @@
+/**
+ * @module
+ * This file acts similarly to "dev_dependencies" in package.json.
+ * Dependencies can be quickly updated by running `deno task up`.
+ *
+ * The versions are defined in the `deno.jsonc` file.
+ */
+import "@deno/dnt";
+import "@std/assert";
+import "@std/cli";
+import "@std/testing/mock";
